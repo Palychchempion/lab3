960d62408f0f7bbfa811a7ee786481f51010b9a0
Palychchempion <148390214+Palychchempion@users.noreply.github.com>
2023-11-07 20:16:03 +0300

    Update lab3.2.py

--- лр3/lab3.2.py
+++ лр3/lab3.2.py
@@ -1,16 +1,16 @@
- # Выполнение второго задания
-def group(group1, group2):
-    group1 = group1.split(",")
-    group2 = group2.split(",")
-    group3 = []
-    for i in group1:
-        for j in group2:
-            if i == j:
-                group3.append(i)
-    group3.sort()
-    return group3
+# TODO Напишите функцию find_common_participants
+
+def find_common_participants(first_group,second_group,splitter=','):
+    set_1 = set(first_group.split(splitter))
+    set_2 = second_group.split(splitter)
+    end_list = list(set_1.intersection(set_2))
+    end_list.sort()
+    return end_list
 
 
-Students = "Дамир,Марвин,Данил,Илюха,Никита"
-Sportsmens = "Дамир,Олег,Никита"
-print(group(Students, Sportsmens))
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group,participants_second_group,'|'))
+
+# TODO Проверьте работу функции с разделителем отличным от запятой
